b340f02e034cd9c40c7b376c902ff9e5668d8992
Yujun Qin <yamstudio@users.noreply.github.com>
2022-07-16 14:54:07 -0400

    Set up Node.js (#3)
    * Set up Node.js
    
    * Add yarn install to action

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const app = (): JSX.Element => {
+    return <h1>Hello world!</h1>;
+};
+
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
